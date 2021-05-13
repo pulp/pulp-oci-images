742f4ef0c86455434b3959486a070bc6d7cdc4de
David Davis <daviddavis@redhat.com>
2021-05-13 12:36:14 -0400

    Add a test image check to publish workflow
    fixes #80

---
+++ assets/test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+cleanup() {
+  docker stop pulp
+}
+trap cleanup EXIT
+
+
+image=${1:-pulp/pulp:latest}
+
+mkdir settings
+echo "CONTENT_ORIGIN='http://localhost:8080'" >> settings/settings.py
+docker run --detach \
+           --name pulp \
+           --publish 8080:80 \
+           --volume "/$(pwd)/settings:/etc/pulp:Z" \
+           --device /dev/fuse \
+           "$image"
+sleep 10
+for _ in $(seq 10)
+do
+  sleep 3
+  if curl --fail http://localhost:8080/pulp/api/v3/status/ > /dev/null 2>&1
+  then
+    break
+  fi
+done
+curl --fail http://localhost:8080/pulp/api/v3/status/
