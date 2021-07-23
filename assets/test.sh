fe487bebaf9e21555bf2c2e7950f2f554c04d284
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-07-23 14:38:17 -0300

    Run pulp container on https
    [noissue]

--- assets/test.sh
+++ assets/test.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
+set -eu
 
 cleanup() {
+  echo ::group::INFO
+  docker exec pulp bash -c "pip3 list && pip3 install pipdeptree && pipdeptree"
+  docker logs pulp
+  echo ::endgroup::
   docker stop pulp
 }
 trap cleanup EXIT
 
 
 image=${1:-pulp/pulp:latest}
+scheme=${2:-http}
+if [[ "$scheme" == "http" ]]; then
+  port=80
+else
+  port=443
+fi
 
 mkdir settings
-echo "CONTENT_ORIGIN='http://localhost:8080'" >> settings/settings.py
+echo "CONTENT_ORIGIN='$scheme://localhost:8080'" >> settings/settings.py
 docker run --detach \
            --name pulp \
-           --publish 8080:80 \
+           --publish 8080:$port \
            --volume "/$(pwd)/settings:/etc/pulp:Z" \
            --device /dev/fuse \
            "$image"
@@ -20,9 +31,9 @@ sleep 10
 for _ in $(seq 20)
 do
   sleep 3
-  if curl --fail http://localhost:8080/pulp/api/v3/status/ > /dev/null 2>&1
+  if curl --insecure --fail $scheme://localhost:8080/pulp/api/v3/status/ > /dev/null 2>&1
   then
     break
   fi
 done
-curl --fail http://localhost:8080/pulp/api/v3/status/
+curl --insecure --fail $scheme://localhost:8080/pulp/api/v3/status/ | jq
