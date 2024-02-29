47bf62cbce2d77cd5bc3d42b66f56b57b956e153
Gerrod Ubben <gerrodubben@gmail.com>
2024-02-29 17:33:12 -0500

    Overhaul CI to be more modular & readable
    [noissue]

---
+++ .ci/scripts/find_pulpcore_version.py
@@ -0,0 +1,41 @@
+import argparse
+import requests
+from packaging.version import parse
+import configparser
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--branch",
+        required=True,
+        help="The branch we are trying to find the latest pulpcore z version on",
+    )
+    opts = parser.parse_args()
+    version = ""
+
+    if opts.branch == "main":
+        # Used for nightly image, find the pulpcore version from @main
+        r = requests.get("https://raw.githubusercontent.com/pulp/pulpcore/main/.bumpversion.cfg")
+        if r.status_code != 200:
+            config = configparser.ConfigParser()
+            config.read_string(r.text)
+            if "bumpversion" in config:
+                version = config["bumpversion"]["current_version"]
+    else:
+        r = requests.get("https://pypi.org/pypi/pulpcore/json")
+        if r.status_code != 200:
+            metadata = r.json()
+            if opts.branch == "latest":
+                version = metadata["info"]["version"]
+            else:
+                releases = metadata["releases"]
+                branch_version = parse(opts.branch)
+                max_z_version = branch_version
+                for version_str in releases.keys():
+                    version = parse(version_str)
+                    if version.major == branch_version.major and version.minor == branch_version.minor:
+                        if version > max_z_version:
+                            max_z_version = version
+                version = f"{max_z_version.major}.{max_z_version.minor}.{max_z_version.micro}"
+    print(version)
