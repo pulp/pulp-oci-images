8ea49651cdba53814a4f23eacf532cc5fa6e739d
Gerrod Ubben <gerrodubben@gmail.com>
2024-07-09 17:36:24 -0400

    Fix docker-compose add_signing_service script
    fixes: #647

--- images/assets/add_signing_service.sh
+++ images/assets/add_signing_service.sh
@@ -3,7 +3,7 @@
 /usr/bin/wait_on_postgres.py
 /usr/bin/wait_on_database_migrations.sh
 
-if [ -n "${PULP_SIGNING_KEY_FINGERPRINT}" ]; then
+if [ -n "${PULP_SIGNING_KEY_FINGERPRINT-}" ]; then
   /usr/local/bin/pulpcore-manager add-signing-service "${COLLECTION_SIGNING_SERVICE}" /var/lib/pulp/scripts/collection_sign.sh "${PULP_SIGNING_KEY_FINGERPRINT}"
   /usr/local/bin/pulpcore-manager add-signing-service "${CONTAINER_SIGNING_SERVICE}" /var/lib/pulp/scripts/container_sign.sh "${PULP_SIGNING_KEY_FINGERPRINT}" --class container:ManifestSigningService
 fi
