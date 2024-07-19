0a3ab9e483fc1b520e3fcea5c19d48cf50901405
Matthias Dellweg <mdellweg@redhat.com>
2024-07-19 10:53:58 +0200

    Install some extra python packages
    It seems like python-nginx was needed by the operator.
    
    Fixes #653

--- images/assets/route_paths.py
+++ images/assets/route_paths.py
@@ -4,15 +4,18 @@ import pkgutil
 import json
 import re
 import sys
-import nginx
 from pathlib import Path
+
+import nginx
 
 nginx_configs = []
 
 for i in pkgutil.iter_modules():
     # filter pulp and galaxy packages
-    if re.search(r'pulp_.*|galaxy_ng',i.name):
-        snippet_file = i.module_finder.path + "/" + i.name + "/app/webserver_snippets/nginx.conf"
+    if re.search(r"pulp_.*|galaxy_ng", i.name):
+        snippet_file = (
+            i.module_finder.path + "/" + i.name + "/app/webserver_snippets/nginx.conf"
+        )
         # only add the filename to array if it exists
         if Path(snippet_file).is_file():
             nginx_configs.append(snippet_file)
@@ -21,7 +24,11 @@ name = sys.argv[1]
 router = []
 
 for plugin_conf in nginx_configs:
-    app = "galaxy" if "galaxy" in plugin_conf else plugin_conf.split("pulp_")[1].split("/")[0]
+    app = (
+        "galaxy"
+        if "galaxy" in plugin_conf
+        else plugin_conf.split("pulp_")[1].split("/")[0]
+    )
     conf = nginx.loadf(plugin_conf)
     for location in conf.filter("Location"):
         path = location.value
@@ -47,20 +54,22 @@ for plugin_conf in nginx_configs:
             raise RuntimeError(f"Location {path} doesn't have proxy_pass")
 
         new_path = {
-            "name":f"{name}-{app}{path.rstrip('/').replace('/', '-').replace('_', '-')}",
+            "name": f"{name}-{app}{path.rstrip('/').replace('/', '-').replace('_', '-')}",
             "path": path,
             "targetPort": target_port,
             "serviceName": svc_name,
         }
         if rewrite:
             new_path["rewrite"] = rewrite
-            router.append({
-                "name":f"{name}-{app}{path.rstrip('/').replace('/', '-').replace('_', '-')}2",
-                "path": path.rstrip("/"),
-                "targetPort": target_port,
-                "serviceName": svc_name,
-                "rewrite": rewrite,
-            })
+            router.append(
+                {
+                    "name": f"{name}-{app}{path.rstrip('/').replace('/', '-').replace('_', '-')}2",
+                    "path": path.rstrip("/"),
+                    "targetPort": target_port,
+                    "serviceName": svc_name,
+                    "rewrite": rewrite,
+                }
+            )
 
         router.append(new_path)
 
